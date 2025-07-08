c351b7cfadbdda935e4fdb7dba2d546dbeafb04a
HRG-Developer <Stephan.Leins@hrg-hotels.com>
2025-07-08 14:48:17 +0200

    maps-module - add shared folder to wp-content and switch lang files to this folder

--- wp-content/plugins/revo-hotels-maps/assets/maps-template.php
+++ wp-content/plugins/revo-hotels-maps/assets/maps-template.php
@@ -5,18 +5,20 @@ defined('ABSPATH') or die('No script kiddies please!');
 ?>
 <script>
     var hotelFilterTranslations = {
-        country: "<?php echo esc_js(ucfirst(__('country', 'revo-hotels-maps'))); ?>",
-        city: "<?php echo esc_js(ucfirst(__('city', 'revo-hotels-maps'))); ?>",
-        brand: "<?php echo esc_js(ucfirst(__('brand', 'revo-hotels-maps'))); ?>",
-        noResult: "<?php echo esc_js(ucfirst(__('noResult', 'revo-hotels-maps'))); ?>",
-        searchResultet: "<?php echo esc_js(ucfirst(__('searchResultet', 'revo-hotels-maps'))); ?>",
-        hits: "<?php echo esc_js(ucfirst(__('hits', 'revo-hotels-maps'))); ?>",
-        yourSelection: "<?php echo esc_js(ucfirst(__('yourSelection', 'revo-hotels-maps'))); ?>",
-        consentText: "<?php echo esc_js(ucfirst(__('consentText', 'revo-hotels-maps'))); ?>",
-        consentHeadline: "<?php echo esc_js(ucfirst(__('consentHeadline', 'revo-hotels-maps'))); ?>",
-        showMap: "<?php echo esc_js(ucfirst(__('showMap', 'revo-hotels-maps'))); ?>",
-        noHotelsFound: "<?php echo esc_js(ucfirst(__('noHotelsFound', 'revo-hotels-maps'))); ?>",
-        noResultsFound: "<?php echo esc_js(ucfirst(__('noResultsFound', 'revo-hotels-maps'))); ?>"
+        country: "<?php echo esc_js(ucfirst(__('country', 'revo-plugin-translation'))); ?>",
+        city: "<?php echo esc_js(ucfirst(__('city', 'revo-plugin-translation'))); ?>",
+        brand: "<?php echo esc_js(ucfirst(__('brand', 'revo-plugin-translation'))); ?>",
+        noResult: "<?php echo esc_js(ucfirst(__('noResult', 'revo-plugin-translation'))); ?>",
+        searchResultet: "<?php echo esc_js(ucfirst(__('searchResultet', 'revo-plugin-translation'))); ?>",
+        hits: "<?php echo esc_js(ucfirst(__('hits', 'revo-plugin-translation'))); ?>",
+        yourSelection: "<?php echo esc_js(ucfirst(__('yourSelection', 'revo-plugin-translation'))); ?>",
+        consentText: "<?php echo esc_js(ucfirst(__('consentText', 'revo-plugin-translation'))); ?>",
+        consentHeadline: "<?php echo esc_js(ucfirst(__('consentHeadline', 'revo-plugin-translation'))); ?>",
+        showMap: "<?php echo esc_js(ucfirst(__('showMap', 'revo-plugin-translation'))); ?>",
+        noHotelsFound: "<?php echo esc_js(ucfirst(__('noHotelsFound', 'revo-plugin-translation'))); ?>",
+        noResultsFound: "<?php echo esc_js(ucfirst(__('noResultsFound', 'revo-plugin-translation'))); ?>",
+        discoverMore: "<?php echo esc_js(ucfirst(__('discover-more', 'revo-plugin-translation'))); ?>"
+
     };
 
 </script>
@@ -26,12 +28,14 @@ defined('ABSPATH') or die('No script kiddies please!');
   <div class="btn-wrapper-view">
     <div class="btn-view-container">
       <a id="grid-view-btn" class="btn btn-view" href="#" role="button" aria-label="Switch to grid view">
-        <img class="view-icon" src="<?php echo esc_url(plugins_url('img/list_alt.svg', __FILE__)); ?>" alt="Map view icon" /> Grid View
+        <img class="view-icon" src="<?php echo esc_url(plugins_url('img/list_alt.svg', __FILE__)); ?>" alt="Map view icon" /> 
+        <?php echo ucfirst(esc_html__('grid-view', 'revo-plugin-translation')) ?>
       </a>
     </div> 
     <div class="btn-view-container">
       <a id="maps-view-btn" class="btn btn-view active-view" href="#" role="button" aria-label="Switch to map view">
-        <img class="view-icon" src="<?php echo esc_url(plugins_url('img/map_active.svg', __FILE__)); ?>" alt="Grid view icon" /> Map View
+        <img class="view-icon" src="<?php echo esc_url(plugins_url('img/map_active.svg', __FILE__)); ?>" alt="Grid view icon" /> 
+        <?php echo ucfirst(esc_html__('map-view', 'revo-plugin-translation')) ?>
       </a>
     </div>
   </div>
@@ -43,7 +47,7 @@ defined('ABSPATH') or die('No script kiddies please!');
         <div class="select-header">
           <input name="country" type="text" autocomplete="off" maxlength="30"
                  id="country-header"
-                 placeholder="<?php echo ucfirst(esc_html__('country', 'hotel-portfolio')) ?>"
+                 placeholder="<?php echo ucfirst(esc_html__('country', 'revo-plugin-translation')) ?>"
                  aria-label="Country" 
                  aria-controls="country-options"
                  aria-expanded="false"
@@ -58,7 +62,7 @@ defined('ABSPATH') or die('No script kiddies please!');
       <div class="select-city" id="city-select">
         <div class="select-header">
           <input name="city" type="text" autocomplete="off" id="city-header" maxlength="50"
-                 placeholder="<?php echo ucfirst(esc_html__('city', 'hotel-portfolio')) ?>"
+                 placeholder="<?php echo ucfirst(esc_html__('city', 'revo-plugin-translation')) ?>"
                  aria-label="City"
                  aria-controls="city-options"
                  aria-expanded="false"
@@ -87,7 +91,7 @@ defined('ABSPATH') or die('No script kiddies please!');
       <div class="select-brand" id="brand-select">
         <div class="select-header">
           <input name="brand" type="text" autocomplete="off" id="brand-header" maxlength="50"
-                 placeholder="<?php echo ucfirst(esc_html__('brand', 'hotel-portfolio')) ?>"
+                 placeholder="<?php echo ucfirst(esc_html__('brand', 'revo-plugin-translation')) ?>"
                  aria-label="Brand"
                  aria-controls="brand-options"
                     />
