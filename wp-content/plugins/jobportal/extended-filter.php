6721c3283fc068079ae78a742254d67d5e609c8e
HRG-Developer <Stephan.Leins@hrg-hotels.com>
2025-09-26 14:37:37 +0200

    jobportal - fix language issues

--- wp-content/plugins/jobportal/extended-filter.php
+++ wp-content/plugins/jobportal/extended-filter.php
@@ -7,14 +7,14 @@
     <div class="ext-filter-header" id="ext-filter-head" role="button" tabindex="0"
          aria-controls="ext-filter-cont" aria-expanded="false">
       <p id="ext-filter-heading" class="article-title black">
-        <?php echo esc_html__('More filters', 'jobportal'); ?>
+        <?php echo esc_attr(ucfirst(__('more-filters', 'jobportal'))); ?>
         <i aria-hidden="true"></i>
       </p>
 
       <div class="filter-set red" aria-live="polite">
         <div class="red-circle" id="filter-count">0</div>
         <div class="font-12" style="padding-top: 10px;">
-          <?php echo esc_html__('Filters set', 'jobportal'); ?>
+          <?php echo esc_attr(ucfirst(__('filters-set', 'jobportal'))); ?>
         </div>
       </div>
 
@@ -31,65 +31,65 @@
     <div class="ext-open accordion-content" id="ext-filter-cont" role="region" aria-labelledby="ext-filter-head">
       <!-- Col 1: Career levels -->
       <div class="extended-search">
-        <p class="heading black"><?php echo esc_html__('Career levels', 'jobportal'); ?>:</p>
+        <p class="heading black"><?php echo esc_attr(ucfirst(__('Career levels', 'jobportal'))); ?>:</p>
         <div class="acc-col">
           <!-- name-Attribute bleiben deine kanonischen Werte (für URL/Filter) -->
           <div class="badge careerlevels" name="Ausbildung" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Apprenticeship', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('apprenticeship', 'jobportal'))); ?></p>
           </div>
           <div class="badge careerlevels" name="Student" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Student jobs', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('student-jobs', 'jobportal'))); ?></p>
           </div>
           <div class="badge careerlevels" name="berufseinsteiger" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Career starters', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('career-starters', 'jobportal'))); ?></p>
           </div>
           <div class="badge careerlevels" name="berufserfahren" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Experienced professionals', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('experienced-professionals', 'jobportal'))); ?></p>
           </div>
           <div class="badge careerlevels" name="quereinsteiger" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Career changer', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('career-changer', 'jobportal'))); ?></p>
           </div>
         </div>
       </div>
 
       <!-- Col 2: Employment type -->
       <div class="extended-search">
-        <p class="heading black"><?php echo esc_html__('Employment type', 'jobportal'); ?>:</p>
+        <p class="heading black"><?php echo esc_attr(ucfirst(__('Employment type', 'jobportal'))); ?>:</p>
         <div class="acc-col">
           <div class="badge employment-type" name="minijob" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Minijob', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('Minijob', 'jobportal'))); ?></p>
           </div>
           <div class="badge employment-type" name="teilzeit" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Part-time', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('part-time', 'jobportal'))); ?></p>
           </div>
           <div class="badge employment-type" name="vollzeit" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Full-time', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('full-time', 'jobportal'))); ?></p>
           </div>
           <div class="badge employment-type" name="stundenweise" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Hourly', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('hourly', 'jobportal'))); ?></p>
           </div>
         </div>
       </div>
 
       <!-- Col 3: Job location type -->
       <div class="extended-search">
-        <p class="heading black"><?php echo esc_html__('Job location type', 'jobportal'); ?>:</p>
+        <p class="heading black"><?php echo esc_attr(ucfirst(__('Job location type', 'jobportal'))); ?>:</p>
         <div class="acc-col">
           <div class="badge joblocation-type" name="remote" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Remote', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('Remote', 'jobportal'))); ?></p>
           </div>
           <div class="badge joblocation-type" name="präsenz" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('On-site', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('on-site', 'jobportal'))); ?></p>
           </div>
           <div class="badge joblocation-type" name="hybrid" role="checkbox" tabindex="0" aria-checked="false">
-            <p><?php echo esc_html__('Hybrid', 'jobportal'); ?></p>
+            <p><?php echo esc_attr(ucfirst(__('Hybrid', 'jobportal'))); ?></p>
           </div>
         </div>
       </div>
 
       <!-- Col 4: (optional) Keywords -->
       <div class="extended-search black" style="display:none;">
-        <p class="heading black"><?php echo esc_html__('Keywords', 'jobportal'); ?>:</p>
+        <p class="heading black"><?php echo esc_attr(ucfirst(__('Keywords', 'jobportal'))); ?>:</p>
         <div class="acc-col">
           <div class="badge keyword" name="keyword-01" role="checkbox" tabindex="0" aria-checked="false">
             <p>keyword-01</p>
